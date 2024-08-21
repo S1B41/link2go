b2860c02b530ab0e4798dd0a1e5c975d5a2c448f
Sibai Mousa <sibai@pm.me>
2024-08-21 20:00:25 +0200

    init FE

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import Home from "./components/home.vue";
+export default {
+  name: "App",
+  components: {
+    Home,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Link2Go</a>
+    </nav>
+    <div class="container">
+     <home />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
