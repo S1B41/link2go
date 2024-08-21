9cd5ed575b3057a8b9e3796a39189ae93ec74664
Sibai Mousa <sibai@pm.me>
2024-08-21 19:49:47 +0200

    prettier

--- server/models/url.js
+++ server/models/url.js
@@ -1,19 +1,19 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from "mongoose"
 
 const urlSchema = new Schema({
-    longURL: {
-        type: String,
-        required: true
-    },
-    shortURL: {
-        type: String,
-        required: true,
-    },
-    shortUrlId: {
-        type: String,
-        required: true,
-        unique: true
-    }
-});
+  longURL: {
+    type: String,
+    required: true,
+  },
+  shortURL: {
+    type: String,
+    required: true,
+  },
+  shortUrlId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+})
 
-export default model("URL", urlSchema);
+export default model("URL", urlSchema)
