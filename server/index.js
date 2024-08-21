45490a40f102d6df1de41710ab7f3e835a0de53e
Sibai Mousa <sibai@pm.me>
2024-08-21 19:56:41 +0200

    prettier

--- server/index.js
+++ server/index.js
@@ -1,42 +1,43 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import {validateUrl, generateUrlKey} from "./services/urlService.js";
-import urlController from "./controllers/urlController";
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import { validateUrl, generateUrlKey } from "./services/urlService.js"
+import urlController from "./controllers/urlController"
 
-const app = express();
-const host = 'link2go.cc'
-dotenv.config();
+const app = express()
+const host = "link2go.cc"
+dotenv.config()
 
 app.use(cors()) // to allow cross origin requests
 app.use(bodyParser.json()) // to convert the request into JSON
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/xwww-
 app.get("/", (req, res) => {
-    res.send("Hello");
-  });
+  res.send("Hello")
+})
 
 app.post("/url", async (req, res) => {
-    try {
-        if (!!validateUrl(req.body.url))
-            return res.status(400).send({ msg: "Invalid URL." });
-        const urlKey = generateUrlKey();
-        const shortUrl = `https://${host}/${urlKey}`
-        await urlController.save(req.body.url, shortUrl, urlKey)
-        return res.status(200).send({ shortUrl });
-    } catch (error) {
-        return res.status(500).send({ msg: "Error. Please try again." });
-    }
-}); 
+  try {
+    if (!!validateUrl(req.body.url))
+      return res.status(400).send({ msg: "Invalid URL." })
+    const urlKey = generateUrlKey()
+    const shortUrl = `https://${host}/${urlKey}`
+    await urlController.save(req.body.url, shortUrl, urlKey)
+    return res.status(200).send({ shortUrl })
+  } catch (error) {
+    return res.status(500).send({ msg: "Error. Please try again." })
+  }
+})
 
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('MongoDB database Connected...'))
-    .catch((err) => console.log(err))
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB database Connected..."))
+  .catch((err) => console.log(err))
 
-app.listen(process.env.PORT, () => console.log(`App listening at http://localhost:${process.env.PORT}`))
-
+app.listen(process.env.PORT, () =>
+  console.log(`App listening at http://localhost:${process.env.PORT}`)
+)
